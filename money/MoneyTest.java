42017f7644cb2b0847ec4c79edf184f687cb49ee
Einosuke-Iida <Einosuke.Iida@ibm.com>
2025-04-05 16:49:06 +0900

    feat: 等価性比較の実装

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -1,5 +1,7 @@
 package money;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +12,11 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         Dollar ten = five.times(2);
         assertEquals(10, ten.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
