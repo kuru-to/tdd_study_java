d2084a01dce2aadafa35705245ed0fc87c79cfb7
Einosuke-Iida <Einosuke.Iida@ibm.com>
2025-04-05 16:41:44 +0900

    feat: 最初のテストを通した

---
+++ money/MoneyTest.java
@@ -0,0 +1,14 @@
+package money;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar ten = five.times(2);
+        assertEquals(10, ten.amount);
+    }
+}
